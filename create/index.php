da0bcbc1d617bb052071b495980d72fb2a552901
frostkom <martin.fors@frostkom.se>
2018-03-05 11:10:40 +0100

    Hide dialog box if no content is there

--- create/index.php
+++ create/index.php
@@ -182,13 +182,14 @@ echo "<div class='wrap'>
 							}
 
 						echo "</div>
-					</div>
-					<div class='postbox'>
-						<h3 class='hndle'><span>".__("About the Group", 'lang_group')."</span></h3>
-						<div class='inside'>";
+					</div>";
 
-							if(is_plugin_active("mf_email/index.php"))
-							{
+					if(is_plugin_active("mf_email/index.php"))
+					{
+						echo "<div class='postbox'>
+							<h3 class='hndle'><span>".__("About the Group", 'lang_group')."</span></h3>
+							<div class='inside'>";
+
 								$obj_email = new mf_email();
 								//$arr_data_incoming = $obj_email->get_from_for_select(array('type' => 'incoming'));
 								$arr_data_email = $obj_email->get_from_for_select();
@@ -203,12 +204,13 @@ echo "<div class='wrap'>
 								.show_select(array('data' => $arr_data_abuse, 'name' => 'intGroupAbuseEmail', 'text' => __("Abuse", 'lang_group'), 'description' => sprintf(__("You should have setup both %s and %s because these addresses are usually used for other servers when sending notices about spam. This is a great way of receiving and handling possible issues within your own domain", 'lang_group'), "abuse@domain.com", "postmaster@domain.com")))
 								.show_select(array('data' => $arr_data_page, 'name' => 'intGroupHelpPage', 'text' => __("Help Page", 'lang_group'), 'value' => $intGroupHelpPage))
 								.show_select(array('data' => $arr_data_page, 'name' => 'intGroupArchivePage', 'text' => __("Archive Page", 'lang_group'), 'value' => $intGroupArchivePage));
-							}
 
-						echo "</div>
-					</div>
-				</div>
-				<div id='postbox-container-1'>
+							echo "</div>
+						</div>
+					</div>";
+				}
+
+				echo "<div id='postbox-container-1'>
 					<div class='postbox'>
 						<div class='inside'>"
 							.show_button(array('name' => "btnGroupCreate", 'text' => __("Save", 'lang_group')))
