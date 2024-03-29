50ec108bfabf140a2da59a8ed768c6cefecee6d9
Martin Fors <martin.fors@frostkom.se>
2024-03-29 20:49:06 +0100

    Use new function for setting time limit

--- README.md
+++
@@ -1,9 +0,0 @@
-## Description
-Wordpress plugin to manage and send group messages.
-
-## Installation
-1. Upload the folder to /wp-content/plugins/
-2. Activate the plugin through Plugins
-
-## Dependencies
-MF Base, MF Address Book
--- include/classes.php
+++ include/classes.php
@@ -1483,7 +1483,7 @@ class mf_group
 		$setting_key = get_setting_key(__FUNCTION__);
 		$option = get_option($setting_key, 'no');
 
-		$description = setting_time_limit(array('key' => $setting_key, 'value' => $option));
+		list($option, $description) = setting_time_limit(array('key' => $setting_key, 'value' => $option, 'return' => 'array'));
 
 		echo show_select(array('data' => get_yes_no_for_select(), 'name' => $setting_key, 'value' => $option, 'description' => $description));
 	}
