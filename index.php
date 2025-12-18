b27555804f4183a84fb57bacc754cdf2b662cf5a
frostkom <martin.fors@frostkom.se>
2025-12-18 09:24:38 +0100

    Standardize localization

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Group
 Plugin URI: https://github.com/frostkom/mf_group
 Description:
-Version: 5.13.35
+Version: 5.13.36
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
